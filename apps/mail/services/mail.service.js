ed39d06d0416dd4e21d75a762c9d250632efa112
LeanneBoro <leanneboro@gmail.com>
2024-03-13 10:56:21 +0200

    Init service file

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,100 @@
-// mail service
+
+import { utilService } from '../services/utilService.js'
+import { storageService } from '../services/async-storage.service.js'
+
+const MAIL_KEY = 'mailDB'
+
+export const carService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyCar,
+    getDefaultFilter
+}
+
+_createBooks()
+
+function query(filterBy = getDefaultFilter()) {
+
+
+    return storageService.query(MAIL_KEY)
+        .then(mails => {
+            // if (filterBy.txt) {
+            //     const regex = new RegExp(filterBy.txt, 'i')
+            //     cars = cars.filter(car => regex.test(car.vendor))
+            // }
+            // if (filterBy.minSpeed) {
+            //     cars = cars.filter(car => car.maxSpeed >= filterBy.minSpeed)
+            // }
+            // if (filterBy.desc) {
+            //     const regex = new RegExp(filterBy.desc, 'i')
+            //     cars = cars.filter(car => regex.test(car.desc))
+            // }
+            return mails
+        })
+}
+
+function get(mailId) {
+    return storageService.get(MAIL_KEY, mailId)
+        .then(mail => _setNextPrevCarId(mail))
+}
+
+
+function remove(mailId) {
+    return storageService.remove(MAIL_KEY, mailId)
+}
+
+function save(mail) {
+    if (mail.id) {
+        return storageService.put(MAIL_KEY, mail)
+    } else {
+        car = _createMail()
+        return storageService.post(MAIL_KEY, mail)
+    }
+}
+
+function getEmptyCar(vendor = '', maxSpeed = '') {
+    return { vendor, maxSpeed }
+}
+
+function getDefaultFilter() {
+    return { txt: '', minSpeed: 50, desc: '' }
+}
+
+
+function _createBooks() {
+    let mails = utilService.loadFromStorage(MAIL_KEY)
+    if (!mails || !mails.length) {
+        mails = [{
+            id: utilService.makeId(),
+            subject: 'Miss you!',
+            body: 'Would love to catch up sometimes',
+            isRead: false,
+            sentAt: 1551133930594,
+            removedAt: null,
+            from: 'momo@momo.com',
+            to: 'user@appsus.com'
+        },
+        {
+            id: utilService.makeId(),
+            subject: 'Hey!',
+            body: 'I just met you... and this is crazy',
+            isRead: false,
+            sentAt: 1551133930594,
+            removedAt: null,
+            from: 'dingo@momo.com',
+            to: 'user@appsus.com'
+        },
+        {
+            id: utilService.makeId(),
+            subject: 'Ummm',
+            body: 'How do i use this?',
+            isRead: false,
+            sentAt: 1551133930594,
+            removedAt: null,
+            from: 'user@appsus.com',
+            to: 'dingo@momo.com'
+        },]
+    }
+}
