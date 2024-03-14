cc1e04294894d16e32f0d12554671c3ab58cd251
Yifaht Meir <yifahtmyers@gmail.com>
2024-03-14 19:19:05 +0200

    Add action and pin btns

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -4,29 +4,48 @@ const { Link, useSearchParams } = ReactRouterDOM
 import { NoteEdit } from '../cmps/NoteEdit.jsx'
 import { DynamicCmp } from '../cmps/dynamic-inputs/DynamicCmp.jsx'
 
+import { noteService } from "../services/note.service.js"
+
 export function NotePreview({ note, onRemoveNote, onUpdateNote }) {
     const [cmpType, setCmpType] = useState('color')
     const [previewStyle, setPreviewStyle] = useState({ backgroundColor: 'white' })
     const [isEditing, setIsEditing] = useState(false)
-    // const [isPinned, setIsPinned] = useState(false)
+    const [noteToEdit, setNoteToEdit] = useState(note)
+    const [isPinned, setIsPinned] = useState(note.isPinned)
+
+    useEffect(() => {
+        // note.isPinned = noteIsPinned
+
+    }, [])
 
     function onChangeStyle(newStyle) {
         setPreviewStyle((prevStyle) => ({ ...prevStyle, ...newStyle }))
     }
 
     if (!note) return <div>loading...</div>
-    return <article style={previewStyle} onClick={() => setIsEditing(prevIsEd => true)} className="note-preview flex column">
-        <h2>{note.title}</h2>
-        <p>{note.desc}</p>
-        <section>
-            <select onChange={(ev) => { setCmpType(ev.target.value) }}>
-                <option value="none" select>None</option>
-                <option value="color">Color</option>
-            </select>
-        </section>
-        <DynamicCmp cmpType={cmpType} onChangeStyle={onChangeStyle} previewStyle={previewStyle} />
+    return <article style={previewStyle} onClick={() => setIsEditing(true)} className="note-preview flex column">
+
+        <button className="pin" onClick={() => {
+            onTogglePin()
+            setIsPinned((isPin) => !isPin)
+            onTogglePin(isPinned)
+        }}>pin</button>
+        {!isEditing && <React.Fragment>
+            <h2>{note.title}</h2>
+            <p>{note.desc}</p>
+        </React.Fragment>
+        }
 
         <div className="note-actions">
+            <button value="color" onClick={(ev) => { setCmpType(ev.target.value) }} >
+                <DynamicCmp cmpType={cmpType} onChangeStyle={onChangeStyle} previewStyle={previewStyle} />
+                Color
+            </button>
+            <button>Picture</button>
+            <button>Video</button>
+            <button>Todo List</button>
+            <button>Copy</button>
+            <button>Email</button>
             <button className="remove-btn" onClick={() => onRemoveNote(note.id)}>X</button>
         </div>
         {isEditing && <NoteEdit
