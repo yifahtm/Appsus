338060df88a8fc8e850d6b583eeed62063631030
LeanneBoro <leanneboro@gmail.com>
2024-03-16 23:21:44 +0200

    Ok i fixed this

--- cmps/Team.jsx
+++ cmps/Team.jsx
@@ -5,9 +5,8 @@ export function Team(){
     return (
         <section className="about">
         <h1>Our Team</h1>
-        <p>what else is there to say about Leanne and Yifaht? Talented, Brilliant, Showstopping,
-            Programmers the world has never seen before. And humble! We both made this app together with 
-            the shared values of "fuck it, we ball". it is truly inspiring to see what such teamwork can accomplish.
+        <p>what is there to say about Leanne and Yifaht? We both made this app together with 
+            the shared values of hard work, determination and love of programming. t is truly inspiring to see what such teamwork can accomplish.
             we hope you enjoy this app and all it has to offer. 
         </p>
         <div className="img-team">
--- cmps/Vision.jsx
+++ cmps/Vision.jsx
@@ -3,12 +3,10 @@
 export function Vision() {
     return (
         <section className="about">
-            <h1>"I'm a boss ass bitch, bitch, bitch, bitch, bitch, bitch, bitch"</h1>
-            <h2>William Shakespeare, probably</h2>
+            <h2>Our vision</h2>
             <p>Our vision at AppSus has always been about spreading joy and positivity. we do it through
-                making apps people everywhere can not use, because this is not a real company and and im just 
-                making stuff up. im very tired ok?? if you read this until the end i owe you 1 million dollars.
-                ok thank you bye.
+                making apps people everywhere can use, bringing them together and helping them express their ideas
+                and creativity. 
             </p>
             <img src="./assets/img/giphy.gif"></img>
         </section>
