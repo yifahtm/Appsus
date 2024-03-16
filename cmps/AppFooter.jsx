c0b90c2b93c506916512fd6614d1c5c7d0585e30
Yifaht Meir <yifahtmyers@gmail.com>
2024-03-16 15:37:23 +0200

    Style footer

--- cmps/AppFooter.jsx
+++ cmps/AppFooter.jsx
@@ -1,10 +1,14 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link } = ReactRouterDOM
 
 export function AppFooter() {
 
-    return <header className="app-header">
+    return <footer className="app-footer flex justify-center">
         <Link to="/">
-            <h3>LOGO!</h3>
+            <h3 className="logo footer-logo flex wrap">AppSUS<i className="fa-solid fa-shield-dog"></i></h3>
+            <p>Presented by
+                <span>Yifaht Meir</span> & <span>Leanne Borowitz</span></p>
+            <small class="footer-rights">@All Rights Reserved 2024 - AppSUS.com</small>
         </Link>
-    </header>
+
+    </footer>
 }
